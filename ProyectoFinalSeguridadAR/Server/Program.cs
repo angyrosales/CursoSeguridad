2d62fa4eb30d423236c53883879620e94190b21f
angyrosales <angy14rosales@gmail.com>
2023-11-16 17:46:08 -0600

    Correciones por Sonarqube

--- ProyectoFinalSeguridadAR/Server/Program.cs
+++ ProyectoFinalSeguridadAR/Server/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace ProyectoFinalSeguridadAR
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
